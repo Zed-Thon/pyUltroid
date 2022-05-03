fd9f7d05d0ca5f6beb7b151578ed017ce0a56416
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-03 20:36:36 +0300

    Update pyUltroid-fns.py

--- pyUltroid-fns.py
+++ pyUltroid-fns.py
@@ -9,17 +9,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 long_description = "# pyUltroid-fns"
 
 name = "pyUltroid-fns"
-author = "TeamUltroid"
+author = "Zed-Thon"
 author_email = "teamultroid@protonmail.ch"
 description = "Function based library for telegram telethon projects."
 license_ = "GNU AFFERO GENERAL PUBLIC LICENSE (v3)"
 
-url = "https://github.com/TeamUltroid/pyUltroid"
+url = "https://github.com/Zed-Thon/pyUltroid"
 
 project_urls = {
-    "Bug Tracker": "https://github.com/TeamUltroid/pyUltroid/issues",
+    "Bug Tracker": "https://github.com/Zed-Thon/pyUltroid/issues",
     "Documentation": "https://ultroid.tech",
-    "Source Code": "https://github.com/TeamUltroid/pyUltroid",
+    "Source Code": "https://github.com/Zed-Thon/pyUltroid",
 }
 classifiers = [
     "Programming Language :: Python :: 3",
